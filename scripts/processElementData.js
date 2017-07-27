7aed6637e94a3f0514cc5199139cdb2c101131da
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2017-07-26 22:21:21 -0300

    commit inicial

---
+++ scripts/processElementData.js
@@ -0,0 +1,52 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_37TdkAL_EduOAKqB9I73uwActivity"]="./../../Easy/deliveryprocesses/resources/Preparation Phase_31F530DE_c7ac11dc_Activity.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_37TdkAL_EduOAKqB9I73uwActivityDetail"]="./../../Easy/deliveryprocesses/resources/Preparation Phase_31F530DE_c7ac11dc_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_NSW6sQMAEduOAKqB9I73uw,_SoXWEQMAEduOAKqB9I73uwActivity"]="./../../Easy/deliveryprocesses/resources/Sprint n_27D4555C_28c5fd25_Activity.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_NSW6sQMAEduOAKqB9I73uw,_SoXWEQMAEduOAKqB9I73uwActivityDetail"]="./../../Easy/deliveryprocesses/resources/Sprint n_27D4555C_28c5fd25_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_NSW6sQMAEduOAKqB9I73uwActivity"]="./../../Easy/deliveryprocesses/resources/Sprint Phase_E1B0777_a3d1e875_Activity.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQ,_NSW6sQMAEduOAKqB9I73uwActivityDetail"]="./../../Easy/deliveryprocesses/resources/Sprint Phase_E1B0777_a3d1e875_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_9llsAQAvEdubGMceRDupFQActivity"]="./../../Easy/deliveryprocesses/resources/easy_lifecycle_8BEF30AA_8bef30aa_Activity.jpeg"
+contentPage.processPage.elementUrls["_OcAGAHFpEeeQB4FKMAE3Rg"] = ["Estabelecer Orçamento","Easy/tasks/Estabelecer Orcamento_2C8575B2.html"];
+contentPage.processPage.elementUrls["_zbM2QIGBEduKE9hnyImx1Q"] = ["Release work","Easy/deliveryprocesses/Sprint de release_58D4AA9.html"];
+contentPage.processPage.elementUrls["_glbG1QMAEduOAKqB9I73uw"] = ["Revisao da Sprint Review","Easy/tasks/revisao_sprint_E6CEA738.html"];
+contentPage.processPage.elementUrls["_zYT-EQMAEduOAKqB9I73uw"] = ["Plan Release","Easy/tasks/plan_release_80F3BDAA.html"];
+contentPage.processPage.elementUrls["_NSW6sQMAEduOAKqB9I73uw"] = ["Sprint phases","Easy/deliveryprocesses/Sprint Phase_E1B0777.html"];
+contentPage.processPage.elementUrls["_glbG0wMAEduOAKqB9I73uw"] = ["Sprint Planning","Easy/tasks/plan_sprint_122255F4.html"];
+contentPage.processPage.elementUrls["_s5oQoHFqEeeQB4FKMAE3Rg"] = ["Preparar ambiente","Easy/deliveryprocesses/Preparar_ambiente_FDDECB61.html"];
+contentPage.processPage.elementUrls["_glbG0QMAEduOAKqB9I73uw"] = ["Update product backlog","Easy/tasks/update_product_backlog_7D81C56C.html"];
+contentPage.processPage.elementUrls["_glbG2wMAEduOAKqB9I73uw"] = ["Sprint Backlog","Easy/workproducts/sprint_backlog_AEF9CB4A.html"];
+contentPage.processPage.elementUrls["_glbG2QMAEduOAKqB9I73uw"] = ["Stakeholder","Easy/roles/stakeHolder_1207C822.html"];
+contentPage.processPage.elementUrls["_glbG1AMAEduOAKqB9I73uw"] = ["Retrospectiva da Sprint ","Easy/tasks/realizar_retrospectiva_E3220062.html"];
+contentPage.processPage.elementUrls["_reB2UHFfEeeQB4FKMAE3Rg"] = ["Estabelecer Cronograma","Easy/tasks/estabelecer_cronograma_C0803FA4.html"];
+contentPage.processPage.elementUrls["_SoXWEQMAEduOAKqB9I73uw"] = ["Sprint","Easy/deliveryprocesses/Sprint n_27D4555C.html"];
+contentPage.processPage.elementUrls["_glbG0AMAEduOAKqB9I73uw"] = ["Manage problems","Easy/tasks/manage_problems_3EEA5FD9.html"];
+contentPage.processPage.elementUrls["_zYaEtAMAEduOAKqB9I73uw"] = ["Product Backlog","Easy/workproducts/product_backlog_DB75BBD.html"];
+contentPage.processPage.elementUrls["_zYaEsgMAEduOAKqB9I73uw"] = ["Easy Team","Easy/roles/team_1D060AFC.html"];
+contentPage.processPage.elementUrls["_j5pocHFrEeeQB4FKMAE3Rg"] = ["Documento de Ambiente Trabalho","Easy/workproducts/documento_de_ambiente_trabalho_A49DE40.html"];
+contentPage.processPage.elementUrls["_E7q_QHFpEeeQB4FKMAE3Rg"] = ["Analise de Risco","Easy/deliveryprocesses/Analise de Risco_96AA493A.html"];
+contentPage.processPage.elementUrls["_7FGsMANkEduYd-55D-Aiqg"] = ["Iniciar Product Backlog","Easy/tasks/iniciar_product_backlog_A3A332AD.html"];
+contentPage.processPage.elementUrls["_bvv0AHFpEeeQB4FKMAE3Rg"] = ["Planilha de Custo","Easy/workproducts/planilha_de_custo_A45C99BE.html"];
+contentPage.processPage.elementUrls["_glbG1gMAEduOAKqB9I73uw"] = ["Daily Scrum","Easy/tasks/realizar_encontro_diario_391727BD.html"];
+contentPage.processPage.elementUrls["_37TdkAL_EduOAKqB9I73uw"] = ["Preparation Phase","Easy/deliveryprocesses/Preparation Phase_31F530DE.html"];
+contentPage.processPage.elementUrls["_9llsAQAvEdubGMceRDupFQ"] = ["Easy Lifecycle","Easy/deliveryprocesses/easy_lifecycle_8BEF30AA.html"];
+contentPage.processPage.elementUrls["_zYaEsAMAEduOAKqB9I73uw"] = ["Product Owner","Easy/roles/product_owner_D991557B.html"];
+contentPage.processPage.elementUrls["_glbG3AMAEduOAKqB9I73uw"] = ["Product Backlog","Easy/workproducts/product_backlog_DB75BBD.html"];
+contentPage.processPage.elementUrls["_nXmKUANlEduYd-55D-Aiqg"] = ["Daily works","Easy/deliveryprocesses/Daily work_AA24EBA4.html"];
+contentPage.processPage.elementUrls["_glbG2AMAEduOAKqB9I73uw"] = ["Product Owner","Easy/roles/product_owner_D991557B.html"];
+contentPage.processPage.elementUrls["_O6XmcANlEduYd-55D-Aiqg"] = ["Preparation tasks","Easy/deliveryprocesses/Preparation work_C3F77ACA.html"];
+contentPage.processPage.elementUrls["_glbG2gMAEduOAKqB9I73uw"] = ["Scrum Team","Easy/roles/team_1D060AFC.html"];
+contentPage.processPage.elementUrls["_fQRPcHGgEeetMYNV1nEceA"] = ["Diagrama de Gantt","Easy/workproducts/diagrama_de_gantt_EC489332.html"];
+contentPage.processPage.elementUrls["_glbG3QMAEduOAKqB9I73uw"] = ["Product increment","Easy/workproducts/product_increment_9A9BD82A.html"];
+contentPage.processPage.elementUrls["_7FGsMQNkEduYd-55D-Aiqg"] = ["Stakeholder","Easy/roles/stakeHolder_1207C822.html"];
